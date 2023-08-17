67bc2ed821e33676979d97cfc88efe88e15723be
Jay Dalal <jaydalal_12194@yahoo.com>
2023-08-17 22:23:15 +0530

    Added method to serialize using string replacing newtonjson

---
+++ ProcessingUnitWithCSharpSolace/Common/IType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piper.Common
+{
+    public interface IType
+    {
+        string serializeToJsonString();
+        IType deSerializeFromJsonString(string json);
+    }
+}
